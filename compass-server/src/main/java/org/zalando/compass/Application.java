5617bad8a24717e96bcde15c14bd7fde5b47648a
Lukas Niemeier <lukas.niemeier@zalando.de>
2016-12-19 18:22:58 +0100

    allow deployment behind proxy

--- compass-server/src/main/java/org/zalando/compass/Application.java
+++ compass-server/src/main/java/org/zalando/compass/Application.java
@@ -5,15 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.time.Clock;
-
-import static org.springframework.context.annotation.FilterType.REGEX;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
