fc192a0752e407048cd5bc852654a142fbcc10b3
Willi Schönborn <w.schoenborn@gmail.com>
2017-06-11 13:54:50 +0200

    Switched to custom schema

--- src/test/java/org/zalando/compass/domain/persistence/EmbeddedDataSourceConfiguration.java
+++ src/test/java/org/zalando/compass/domain/persistence/EmbeddedDataSourceConfiguration.java
@@ -6,13 +6,26 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collections;
+
+import static java.util.Collections.singletonMap;
 
 @Configuration
 public class EmbeddedDataSourceConfiguration {
 
     @Bean
-    public DataSource dataSource(final EmbeddedPostgres postgres) {
-        return postgres.getPostgresDatabase();
+    public DataSource dataSource(final EmbeddedPostgres postgres) throws SQLException {
+        final DataSource dataSource = postgres.getPostgresDatabase();
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("CREATE SCHEMA compass")) {
+            statement.executeUpdate();
+        }
+
+        return postgres.getPostgresDatabase(singletonMap("currentSchema", "compass"));
     }
 
     @Bean(destroyMethod = "close")
