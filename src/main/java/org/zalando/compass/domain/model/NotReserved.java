2fca4ce9fc59cf96c1e77405b6f82d7ef68783e6
Willi Schönborn <w.schoenborn@gmail.com>
2017-06-27 00:08:13 +0200

    Removed some unused reserved keywords

--- src/main/java/org/zalando/compass/domain/model/NotReserved.java
+++ src/main/java/org/zalando/compass/domain/model/NotReserved.java
@@ -38,20 +38,16 @@ public @interface NotReserved {
                 // http://zalando.github.io/restful-api-guidelines/naming/Naming.html#may-use-conventional-query-strings
                 "q", // default query parameter
                 "limit", // to restrict the number of entries
-                "cursor", //key-based page start
+                "cursor", // key-based page start
                 "offset", // numeric offset page start
-                "sort", //comma-separated list of fields to sort
-                "fields", //to retrieve a subset of fields
+                "sort", // comma-separated list of fields to sort
+                "fields", // to retrieve a subset of fields
                 "embed", // to expand embedded entFities
 
                 // Google Cloud Platform: API Design Guide
                 // https://cloud.google.com/apis/design/standard_fields
                 "filter",
                 "query",
-                "page_token",
-                "page_size",
-                "order_by",
-                "show_deleted",
 
                 // Compass specific
                 "key"
