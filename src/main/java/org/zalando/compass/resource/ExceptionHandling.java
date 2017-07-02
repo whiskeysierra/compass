d5654a9b12ceb45d184232b7aa05b6d5b733005f
Willi Schönborn <w.schoenborn@gmail.com>
2017-07-02 03:31:33 +0200

    Fixed broken build

--- src/main/java/org/zalando/compass/resource/ExceptionHandling.java
+++ src/main/java/org/zalando/compass/resource/ExceptionHandling.java
@@ -1,6 +1,5 @@
 package org.zalando.compass.resource;
 
-import com.google.common.base.CaseFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
@@ -12,8 +11,6 @@ import org.zalando.compass.domain.persistence.NotFoundException;
 import org.zalando.problem.Problem;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 import org.zalando.problem.spring.web.advice.SpringAdviceTrait;
-
-import javax.annotation.Nonnull;
 
 @ControllerAdvice
 class ExceptionHandling implements ProblemHandling, SpringAdviceTrait {
@@ -32,11 +29,6 @@ class ExceptionHandling implements ProblemHandling, SpringAdviceTrait {
             final NativeWebRequest request) throws HttpMediaTypeNotAcceptableException {
 
         return create(HttpStatus.NOT_FOUND, exception, request);
-    }
-
-    @Override
-    public String formatFieldName(@Nonnull final String fieldName) {
-        return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, fieldName);
     }
 
 }
