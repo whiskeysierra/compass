d1cc203153a156b6caa3d1c1304c5f9f4745b8dc
Willi Schönborn <w.schoenborn@gmail.com>
2018-05-13 22:19:51 +0200

    Removed unused import

--- src/test/java/org/zalando/compass/features/HttpSteps.java
+++ src/test/java/org/zalando/compass/features/HttpSteps.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.zalando.riptide.Http;
