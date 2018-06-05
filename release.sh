1f178c62a03aae3b47106d074fe33c66c22067bf
Willi Schönborn <w.schoenborn@gmail.com>
2018-06-06 00:19:40 +0200

    Updated release script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -ex
+
+: ${1?"Usage: $0 <major|minor|patch>"}
+
+./mvnw scm:check-local-modification
+
+current=$(git describe --abbrev=0)
+release=$(semver ${current} -i $1 --preid RC)
+next=$(semver ${release} -i minor)
+
+# release
+./mvnw versions:set -D newVersion=${release}
+git add $(find . -name pom.xml)
+git commit -m "Release ${release}"
+./mvnw clean deploy -P release
+./mvnw scm:tag
+
+# next development version
+./mvnw versions:set -D newVersion=${next}-SNAPSHOT
+git add $(find . -name pom.xml)
+git commit -m "Development ${next}-SNAPSHOT"
+
+git push
+git push --tags
